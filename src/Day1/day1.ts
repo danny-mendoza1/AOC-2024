40cb7f5b6414233505e00ab7a8825f3d58fd9c7a
Walter Mendoza <walter.mendoza@snapsheet.me>
2024-12-04 12:59:24 -0600

    prettier clean up

--- src/Day1/day1.ts
+++ src/Day1/day1.ts
@@ -1,27 +1,26 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const filePath = './src/Day1/day1-input.txt';
+const filePath = "./src/Day1/day1-input.txt";
 try {
-  const data = fs.readFileSync(filePath, 'utf-8');
-  const lines = data.split('\n');
+  const data = fs.readFileSync(filePath, "utf-8");
+  const lines = data.split("\n");
 
   const leftArray: number[] = [];
-  const rightArray: number[]= [];
+  const rightArray: number[] = [];
 
-  lines.forEach(line => {
+  lines.forEach((line) => {
     const [leftNum, rightNum] = line.trim().split(/\s+/);
-    leftArray.push(Number(leftNum))
-    rightArray.push(Number(rightNum))
-  })
+    leftArray.push(Number(leftNum));
+    rightArray.push(Number(rightNum));
+  });
 
   //sort both arrays in ascending order
-  leftArray.sort((a,b) => a-b)
-  rightArray.sort((a,b) => a-b)
+  leftArray.sort((a, b) => a - b);
+  rightArray.sort((a, b) => a - b);
 
-  if(leftArray.length !== rightArray.length) {
-    console.log("Arrays must have the same length")
-  }
-  else {
+  if (leftArray.length !== rightArray.length) {
+    console.log("Arrays must have the same length");
+  } else {
     // let totalDistance = 0;
 
     // for (let i=0; i < leftArray.length; i++) {
@@ -33,23 +32,20 @@ try {
 
     const frequencyMap: { [key: number]: number } = {};
 
-    rightArray.forEach(num => {
+    rightArray.forEach((num) => {
       frequencyMap[num] = (frequencyMap[num] || 0) + 1;
-    })
+    });
 
-    let similarityScore = 0
+    let similarityScore = 0;
 
-    leftArray.forEach(num => {
+    leftArray.forEach((num) => {
       if (frequencyMap[num]) {
-        const occurences = frequencyMap[num]
-        similarityScore += occurences * num
+        const occurences = frequencyMap[num];
+        similarityScore += occurences * num;
       }
-    })
-    console.log("Part 2 similarity score: ", similarityScore)
+    });
+    console.log("Part 2 similarity score: ", similarityScore);
   }
-
 } catch (err) {
-  console.error('Error reading file:', err);
+  console.error("Error reading file:", err);
 }
-
-
