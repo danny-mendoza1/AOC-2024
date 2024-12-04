9c3bcbba684b4b721e5656a476f89c2aceb930a8
Walter Mendoza <walter.mendoza@snapsheet.me>
2024-12-04 11:57:29 -0600

    correctly finds answer for day1 part1

--- src/Day1/day1.ts
+++ src/Day1/day1.ts
@@ -2,13 +2,39 @@ import * as fs from 'fs';
 
 const filePath = './src/Day1/day1-input.txt'; // Correct path format
 
-// Read the file synchronously
 try {
   const data = fs.readFileSync(filePath, 'utf-8');
-  
-  // Store the data in a variable and print it
-  const numbers = data.split('\n').map(line => parseInt(line.trim()));
-  console.log('Parsed Numbers:', numbers);
+  const lines = data.split('\n');
+
+  const leftArray: number[] = [];
+  const rightArray: number[]= [];
+
+  lines.forEach(line => {
+    const [leftNum, rightNum] = line.trim().split(/\s+/);
+    leftArray.push(Number(leftNum))
+    rightArray.push(Number(rightNum))
+  })
+
+  //sort both arrays in ascending order
+  leftArray.sort((a,b) => a-b)
+  rightArray.sort((a,b) => a-b)
+
+  if(leftArray.length !== rightArray.length) {
+    console.log("Arrays must have the same length")
+  }
+  else {
+    let totalDistance = 0;
+
+    for (let i=0; i < leftArray.length; i++) {
+      const distance = Math.abs(leftArray[i] - rightArray[i])
+      totalDistance += distance;
+    }
+
+    console.log("Total distance: ", totalDistance)
+  }
+
 } catch (err) {
   console.error('Error reading file:', err);
 }
+
+
